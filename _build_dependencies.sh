5eb2b01d04b22aa589d26edae461eaafed667a99
Nadyr <41974248+Darlantanis@users.noreply.github.com>
2021-11-03 19:21:12 -0400

    powder that makes you say yes

--- _build_dependencies.sh
+++ _build_dependencies.sh
@@ -6,12 +6,6 @@ export NODE_VERSION=12
 # Byond Major
 export BYOND_MAJOR=514
 # Byond Minor
-<<<<<<< HEAD
-export BYOND_MINOR=1558
-||||||| parent of 09a2377063... Merge pull request #11731 from VOREStation/Arokha/civersion
-export BYOND_MINOR=1557
-=======
 export BYOND_MINOR=1569
->>>>>>> 09a2377063... Merge pull request #11731 from VOREStation/Arokha/civersion
 # Macro Count
 export MACRO_COUNT=4
