ee62bdeb4eb951331501f4e743c2dfdff50032ea
Casey <a.roaming.shadow@gmail.com>
2022-03-18 15:30:21 -0400

    Species in Char Directory

--- tgui/packages/tgui/interfaces/CharacterDirectory.js
+++ tgui/packages/tgui/interfaces/CharacterDirectory.js
@@ -94,6 +94,11 @@ const ViewCharacter = (props, context) => {
         content="Back"
         onClick={() => setOverlay(null)} />
     }>
+      <Section level={2} title="Species">
+        <Box>
+          {overlay.species}
+        </Box>
+      </Section>
       <Section level={2} title="Vore Tag">
         <Box p={1} backgroundColor={getTagColor(overlay.tag)}>
           {overlay.tag}
@@ -149,6 +154,7 @@ const CharacterDirectoryList = (props, context) => {
       <Table>
         <Table.Row bold>
           <SortButton id="name">Name</SortButton>
+          <SortButton id="species">Species</SortButton>
           <SortButton id="tag">Vore Tag</SortButton>
           <SortButton id="erptag">ERP Tag</SortButton>
           <SortButton id="eventtag">Event Pref</SortButton>
@@ -162,6 +168,7 @@ const CharacterDirectoryList = (props, context) => {
           .map((character, i) => (
             <Table.Row key={i} backgroundColor={getTagColor(character.tag)}>
               <Table.Cell p={1}>{character.name}</Table.Cell>
+              <Table.Cell>{character.species}</Table.Cell>
               <Table.Cell>{character.tag}</Table.Cell>
               <Table.Cell>{character.erptag}</Table.Cell>
               <Table.Cell>{character.eventtag}</Table.Cell>
