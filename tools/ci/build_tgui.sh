922cfda58705eed927d4ceacbc0cee716d777e89
Casey <a.roaming.shadow@gmail.com>
2022-07-03 05:04:45 -0400

    GitHub Workflows

--- tools/ci/build_tgui.sh
+++ tools/ci/build_tgui.sh
@@ -7,5 +7,11 @@ source ~/.nvm/nvm.sh
 nvm use $NODE_VERSION
 cd tgui
 chmod +x bin/tgui
+<<<<<<< HEAD
 bin/tgui --ci
+=======
+bin/tgui --lint
+bin/tgui --test
+yarn tgui:prettier
+>>>>>>> b69bb5923c... Merge pull request #13233 from ItsSelis/patch-3
 cd ..
