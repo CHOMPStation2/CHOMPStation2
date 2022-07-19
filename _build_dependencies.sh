3fbff495a768ed8dea6c91ed908a130c8d8d9d17
Casey <a.roaming.shadow@gmail.com>
2022-07-19 04:45:42 -0400

    upstream-merge-8669 Fix

--- _build_dependencies.sh
+++ _build_dependencies.sh
@@ -6,6 +6,10 @@ export NODE_VERSION=16
 # Byond Major
 export BYOND_MAJOR=514
 # Byond Minor
+<<<<<<< HEAD
 export BYOND_MINOR=1571
+=======
+export BYOND_MINOR=1585
+>>>>>>> 2d4f1b82f8... Merge pull request #13363 from ItsSelis/patch-4
 # Macro Count
 export MACRO_COUNT=4
