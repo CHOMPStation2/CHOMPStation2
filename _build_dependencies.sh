b792d7b224ebafda707addddd37fecef879edbcf
Heroman3003 <31296024+Heroman3003@users.noreply.github.com>
2022-12-04 08:41:28 +1000

    [MIRROR] match server byond version for CI

--- _build_dependencies.sh
+++ _build_dependencies.sh
@@ -6,6 +6,10 @@ export NODE_VERSION=16
 # Byond Major
 export BYOND_MAJOR=514
 # Byond Minor
+<<<<<<< HEAD
 export BYOND_MINOR=1571
+=======
+export BYOND_MINOR=1589
+>>>>>>> 9e45b41bae... Merge pull request #13988 from VOREStation/upstream-merge-8791
 # Macro Count
 export MACRO_COUNT=4
