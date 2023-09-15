c107f5492ea7d7ae1538dcceb765348dde328e93
Verkister <superverkelian@hotmail.com>
2023-09-15 18:20:56 +0300

    Adds belly-specific toggle setting for entrance logs Pred's vorgan entrance chat messages (thing slides into thang) can now be toggled off per-belly.

--- tgui/packages/tgui_ch/interfaces/VorePanel.js
+++ tgui/packages/tgui_ch/interfaces/VorePanel.js
@@ -49,7 +49,7 @@ const digestModeToPreyMode = {
  *   show_liq, liq_interacts, liq_reagent_gen, liq_reagent_type, liq_reagent_name,
  *   liq_reagent_transfer_verb, liq_reagent_nutri_rate, liq_reagent_capacity, liq_sloshing, liq_reagent_addons,
  *   show_liq_fullness, liq_messages, liq_msg_toggle1, liq_msg_toggle2, liq_msg_toggle3, liq_msg_toggle4,
- *   liq_msg_toggle5, liq_msg1, liq_msg2, liq_msg3, liq_msg4, liq_msg5, sound_volume, egg_name, recycling,
+ *   liq_msg_toggle5, liq_msg1, liq_msg2, liq_msg3, liq_msg4, liq_msg5, sound_volume, egg_name, recycling, entrance_logs,
  *   custom_reagentcolor, custom_reagentalpha, liquid_overlay, max_liquid_level, mush_overlay, mush_color, mush_alpha, max_mush, min_mush, item_mush_val
  *
  * To the tabs section of VoreSelectedBelly return
@@ -688,6 +688,7 @@ const VoreSelectedBellyOptions = (props, context) => {
     egg_type,
     egg_name,
     recycling,
+    entrance_logs,
     selective_preference,
     save_digest_mode,
     eating_privacy_local,
@@ -890,6 +891,16 @@ const VoreSelectedBellyOptions = (props, context) => {
               icon={recycling ? 'toggle-on' : 'toggle-off'}
               selected={recycling}
               content={recycling ? 'Enabled' : 'Disabled'}
+            />
+          </LabeledList.Item>
+          <LabeledList.Item label="Entrance Logs">
+            <Button
+              onClick={() =>
+                act('set_attribute', { attribute: 'b_entrance_logs' })
+              }
+              icon={entrance_logs ? 'toggle-on' : 'toggle-off'}
+              selected={entrance_logs}
+              content={entrance_logs ? 'Enabled' : 'Disabled'}
             />
           </LabeledList.Item>
           <LabeledList.Item label="Selective Mode Preference">
