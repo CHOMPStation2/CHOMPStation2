945bc44660dc4032011747404c7b3f8004dfd25b
Selis <selis@xynolabs.com>
2024-03-05 22:42:46 +0100

    .

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -9,16 +9,23 @@ if [[ $# -eq 2 ]] ; then
   cd $2
 fi
 
+#mkdir -p \
+#    $1/maps \
+#    $1/icons/effects \
+#    $1/icons/mob/clothing \
+#    $1/icons/mob/inhands \
+#    $1/icons/mob/simple \
+#    $1/icons/obj \
+#    $1/icons/runtime \
+#    $1/sound/runtime \
+#    $1/strings \
+#    $1/tgui/public \
+#    $1/tgui/packages/tgfont/dist
+
 mkdir -p \
     $1/maps \
-    $1/icons/effects \
-    $1/icons/mob/clothing \
-    $1/icons/mob/inhands \
-    $1/icons/mob/simple \
-    $1/icons/obj \
-    $1/icons/runtime \
-    $1/sound/runtime \
-    $1/strings \
+    $1/icons \
+    $1/sound \
     $1/tgui/public \
     $1/tgui/packages/tgfont/dist
 
@@ -29,14 +36,16 @@ fi
 
 cp vorestation.dmb vorestation.rsc $1/
 cp -r maps/* $1/maps/
-cp -r icons/effects/* $1/icons/effects/
-cp -r icons/mob/clothing/* $1/icons/mob/clothing/
-cp -r icons/mob/inhands/* $1/icons/mob/inhands/
-cp -r icons/mob/simple/* $1/icons/mob/simple/
-cp -r icons/obj/* $1/icons/obj/
-cp -r icons/runtime/* $1/icons/runtime/
-cp -r sound/runtime/* $1/sound/runtime/
-cp -r strings/* $1/strings/
+#cp -r icons/effects/* $1/icons/effects/
+#cp -r icons/mob/clothing/* $1/icons/mob/clothing/
+#cp -r icons/mob/inhands/* $1/icons/mob/inhands/
+#cp -r icons/mob/simple/* $1/icons/mob/simple/
+#cp -r icons/obj/* $1/icons/obj/
+#cp -r icons/runtime/* $1/icons/runtime/
+#cp -r sound/runtime/* $1/sound/runtime/
+#cp -r strings/* $1/strings/
+cp -r icons/* $1/icons/
+cp -r sound/* $1/sound/
 cp -r tgui/public/* $1/tgui/public/
 cp -r tgui/packages/tgfont/dist/* $1/tgui/packages/tgfont/dist/
 
