3c703479d336fa802d7b954920b73276673efabb
Selis <selis@xynolabs.com>
2024-03-10 19:46:53 +0100

    checkout change

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -18,14 +18,20 @@ fi
 #    $1/icons/obj \
 #    $1/icons/runtime \
 #    $1/sound/runtime \
-#    $1/strings \
+#    $1/modular_chomp/strings \
 #    $1/tgui/public \
 #    $1/tgui/packages/tgfont/dist
 
 mkdir -p \
     $1/maps \
-    $1/icons \
+    $1/icons/effects \
+		$1/icons/mob/vore \
+		$1/icons/obj \
     $1/sound \
+		$1/modular_chomp/strings \
+		$1/modular_chomp/icons/effects \
+		$1/modular_chomp/icons/mob/vore \
+		$1/modular_chomp/icons/obj \
     $1/tgui/public \
     $1/tgui/packages/tgfont/dist
 
@@ -36,14 +42,17 @@ fi
 
 cp vorestation.dmb vorestation.rsc $1/
 cp -r maps/* $1/maps/
-#cp -r icons/effects/* $1/icons/effects/
+cp -r icons/effects/* $1/icons/effects/
 #cp -r icons/mob/clothing/* $1/icons/mob/clothing/
 #cp -r icons/mob/inhands/* $1/icons/mob/inhands/
 #cp -r icons/mob/simple/* $1/icons/mob/simple/
-#cp -r icons/obj/* $1/icons/obj/
+cp -r icons/obj/* $1/icons/obj/
 #cp -r icons/runtime/* $1/icons/runtime/
 #cp -r sound/runtime/* $1/sound/runtime/
-#cp -r strings/* $1/strings/
+cp -r modular_chomp/icons/effects/* $1/modular_chomp/icons/effects/
+cp -r modular_chomp/icons/mob/vore* $1/modular_chomp/icons/mob/vore/
+cp -r modular_chomp/icons/obj/* $1/modular_chomp/icons/obj/
+cp -r modular_chomp/strings/* $1/modular_chomp/strings/
 cp -r icons/* $1/icons/
 cp -r sound/* $1/sound/
 cp -r tgui/public/* $1/tgui/public/
