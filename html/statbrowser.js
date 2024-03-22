b5e64dfe14f23def32ea5c5e1f251e852cbc04d6
Raeschen <rycoop29@gmail.com>
2024-03-22 16:37:40 +0100

    Revert "fix icon rendering"
    This reverts commit 4bea5a5e65431dafb02246bee15fd4877e0f1fe7.

--- html/statbrowser.js
+++ html/statbrowser.js
@@ -445,6 +445,7 @@ function iconError(e) {
 		node.src = null;
 		node.src = src + '#' + current_attempts;
 		node.setAttribute("data-attempts", current_attempts + 1);
+		draw_listedturf();
 	}, imageRetryDelay);
 }
 
