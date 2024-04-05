c78ac98833cb51a637519dc42a35a527698ecb07
Cadyn Bombaci <cadynspacetechguy@gmail.com>
2024-04-04 17:10:10 -0700

    correct map and some formatting oopsies

--- tools/github-actions/nanomap-renderer-invoker.sh
+++ tools/github-actions/nanomap-renderer-invoker.sh
@@ -2,22 +2,22 @@
 BASEDIR=$PWD
 #Put directories to get maps from here. One per line.
 mapdirs=(
-        "maps/relic_base"
+    "maps/southern_cross"
 )
 #DO NOT TOUCH THIS VARIABLE. It will automatically fill with any maps in mapdirs that are form MAPNAME-n.dmm where n is the z level.
 map_files=()
 
 #Fill up mapfiles list
 for mapdir in ${mapdirs[@]}; do
-        echo "Scanning $mapdir..."
-        FULLMAPDIR=$BASEDIR/$mapdir
-        map_files+=($FULLMAPDIR/*-*[0-9].dmm)
+    echo "Scanning $mapdir..."
+    FULLMAPDIR=$BASEDIR/$mapdir
+    map_files+=($FULLMAPDIR/*-*[0-9].dmm)
 done
 
 #Print full map list
 echo "Full map list:"
 for map in ${map_files[@]}; do
-        echo $map
+    echo $map
 done
 
 printf "\n\n\n"
@@ -33,7 +33,7 @@ echo "Starting image resizing..."
 
 #Resize images to proper size and move them to the correct place
 for map in ./*.png; do
-        j=$(echo $map | sed -n "s/^\.\/\(.*\)-\([0-9]*\)\-1.png$/\1_nanomap_z\2.png/p")
-        echo "Resizing $map and moving to icons/_nanomaps/$j"
-        convert $map -resize 2240x2240 "$BASEDIR/icons/_nanomaps/$j"
+    j=$(echo $map | sed -n "s/^\.\/\(.*\)-\([0-9]*\)\-1.png$/\1_nanomap_z\2.png/p")
+    echo "Resizing $map and moving to icons/_nanomaps/$j"
+    convert $map -resize 2240x2240 "$BASEDIR/icons/_nanomaps/$j"
 done
