ee0d5c5ab8f4c73d8511c462bce49d17f8ebd8df
Kashargul <KashL@t-online.de>
2024-09-23 23:54:07 +0200

    .

--- tools/ci/install_build_deps.sh
+++ tools/ci/install_build_deps.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 source dependencies.sh
 
 source ~/.nvm/nvm.sh
-nvm install $NODE_VERSION
-nvm use $NODE_VERSION
+nvm install $NODE_VERSION_LTS
+nvm use $NODE_VERSION_LTS
 npm install --location=global yarn
 
 pip install --user PyYaml -q
