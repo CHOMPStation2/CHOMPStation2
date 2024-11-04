e292b0cb492b3344264cf1ec330d8e806bc80aa7
CHOMPStation2StaffMirrorBot <94713762+CHOMPStation2StaffMirrorBot@users.noreply.github.com>
2024-11-04 14:00:56 -0700

    [MIRROR] Make vore panel layout better for long belly descriptions in the insi… (#9409)
    Co-authored-by: ShadowLarkens <shadowlarkens@gmail.com>
    Co-authored-by: Kashargul <144968721+Kashargul@users.noreply.github.com>

--- tgui/packages/tgui/interfaces/chompstation/VorePanel/VoreInsidePanel.tsx
+++ tgui/packages/tgui/interfaces/chompstation/VorePanel/VoreInsidePanel.tsx
@@ -31,7 +31,7 @@ export const VoreInsidePanel = (props: {
   }
 
   return (
-    <Section title="Inside">
+    <Section title="Inside" fill scrollable>
       <Box color="green" inline>
         You are currently {absorbed ? 'absorbed into' : 'inside'}
       </Box>
@@ -43,14 +43,14 @@ export const VoreInsidePanel = (props: {
       <Box color="red" inline>
         {belly_name}
       </Box>
-      {liq_lvl > 0 ? (
+      {liq_lvl! > 0 ? (
         <>
           ,&nbsp;
           <Box color="yellow" inline>
             bathing in a pool of
           </Box>
           &nbsp;
-          <Box color={reagentToColor[liq_reagent_type]} inline>
+          <Box color={reagentToColor[liq_reagent_type!]} inline>
             {liuq_name}
           </Box>
         </>
@@ -62,15 +62,15 @@ export const VoreInsidePanel = (props: {
         and you are
       </Box>
       &nbsp;
-      <Box color={digestModeToColor[belly_mode]} inline>
-        {digestModeToPreyMode[belly_mode]}
+      <Box color={digestModeToColor[belly_mode!]} inline>
+        {digestModeToPreyMode[belly_mode!]}
       </Box>
       &nbsp;
       <Box color="label">{desc}</Box>
-      {(contents.length && (
+      {(contents!.length && (
         <Collapsible title="Belly Contents">
           <VoreContentsPanel
-            contents={contents}
+            contents={contents!}
             belly={ref}
             show_pictures={show_pictures}
             icon_overflow={icon_overflow}
