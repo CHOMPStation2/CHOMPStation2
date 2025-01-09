dac3c4ae97f568092352fb20acb9273900f907e4
Drulikar <drathek@gmail.com>
2025-01-08 20:18:40 -0600

    Port mergemap linting (as in you must commit maps with hooks else use a script every edit)

--- tools/mapmerge2/dmm_test.py
+++ tools/mapmerge2/dmm_test.py
@@ -1,8 +1,10 @@
 import os
 import sys
+import pygit2
 
 from tools.mapmerge2 import frontend
 from .dmm import *
+from .mapmerge import merge_map
 
 def green(text):
     return "\033[32m" + str(text) + "\033[0m"
@@ -19,6 +21,25 @@ class LintException(Exception):
     pass
 
 def _self_test():
+    repo = pygit2.Repository(pygit2.discover_repository(os.getcwd()))
+
+    # Read the HEAD and ancestor commits
+    # Assumption: origin on the runner is what we'd normally call upstream
+    ancestor = None
+    ancestor_commit = None
+    try:
+        # if HEAD is a merge commit:
+        ancestor = repo.merge_base(repo.revparse_single("HEAD^2").id, repo.revparse_single("refs/remotes/origin/master").id)
+    except KeyError:
+        # if HEAD isn't a merge commit:
+        ancestor = repo.merge_base(repo.head.target, repo.revparse_single("refs/remotes/origin/master").id)
+        pass
+    if not ancestor:
+        print("Unable to determine merge base!")
+    else:
+        ancestor_commit = repo[ancestor]
+        print("Determined ancestor commit SHA to be:", ancestor)
+
     count = 0
     failed = 0
     for dirpath, dirnames, filenames in os.walk(frontend.read_settings().map_folder):
@@ -29,12 +50,33 @@ def _self_test():
                 fullpath = os.path.join(dirpath, filename)
                 path = fullpath.replace("\\", "/").removeprefix("./")
                 try:
-                    # test: can we load every DMM in the tree
-                    DMM.from_file(fullpath)
+                    # test: can we load every DMM
+                    index_map = DMM.from_file(fullpath)
 
                     # test: is every DMM in TGM format
                     if not has_tgm_header(fullpath):
                         raise LintException('Map is not in TGM format! Please run `/tools/mapmerge2/I Forgot To Map Merge.bat`')
+
+                    # test: does every DMM convert cleanly
+                    if ancestor_commit:
+                        try:
+                            ancestor_blob = ancestor_commit.tree[path]
+                        except KeyError:
+                            # New map, no entry in ancestor
+                            print("New map? Could not find ancestor version of", path)
+                            merged_map = merge_map(index_map, index_map) # basically only tests unused keys
+                            original_bytes = index_map.to_bytes()
+                            merged_bytes = merged_map.to_bytes()
+                            if original_bytes != merged_bytes:
+                                raise LintException('New map is pending updates! Please run `/tools/mapmerge2/I Forgot To Map Merge.bat`')
+                        else:
+                            # Entry in ancestor, merge the index over it
+                            ancestor_map = DMM.from_bytes(ancestor_blob.read_raw())
+                            merged_map = merge_map(index_map, ancestor_map)
+                            original_bytes = index_map.to_bytes()
+                            merged_bytes = merged_map.to_bytes()
+                            if original_bytes != merged_bytes:
+                                raise LintException('Map is pending updates! Please run `/tools/mapmerge2/I Forgot To Map Merge.bat`')
                 except LintException as error:
                     failed += 1
                     print(red(f'Failed on: {path}'))
