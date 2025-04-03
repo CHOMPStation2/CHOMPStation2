ea659ca81867651d9fb956d36d9a0bafa69f6d54
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-04-03 22:54:22 +0200

    dependency update

--- tgui/packages/tgui-panel/chat/renderer.tsx
+++ tgui/packages/tgui-panel/chat/renderer.tsx
@@ -266,18 +266,8 @@ class ChatRenderer {
     else {
       this.rootNode = node;
     }
-    // Find scrollable parent
-    if (this.rootNode) {
-      this.scrollNode = findNearestScrollableParent(
-        this.rootNode,
-      ) as HTMLElement;
-    }
-    if (this.scrollNode) {
-      this.scrollNode.addEventListener('scroll', this.handleScroll);
-    }
-    setTimeout(() => {
-      this.scrollToBottom();
-    });
+    // Attempts to find the scroll node on mount
+    this.tryFindScrollable();
     // Flush the queue
     this.tryFlushQueue();
   }
@@ -291,6 +281,14 @@ class ChatRenderer {
     if (this.isReady() && this.queue.length > 0) {
       this.processBatch(this.queue, { doArchive: doArchive });
       this.queue = [];
+<<<<<<< HEAD
+=======
+      // In case we had no vaclid scroll node before
+      setTimeout(() => {
+        this.tryFindScrollable();
+        this.scrollToBottom();
+      });
+>>>>>>> d1b9aa9ae8 (dependency update (#17454))
     }
   }
 
@@ -449,6 +447,23 @@ class ChatRenderer {
     }
   }
 
+<<<<<<< HEAD
+=======
+  tryFindScrollable() {
+    // Find scrollable parent
+    if (this.rootNode) {
+      if (!this.scrollNode || this.scrollNode.scrollHeight === undefined) {
+        this.scrollNode = findNearestScrollableParent(
+          this.rootNode,
+        ) as HTMLElement;
+        if (this.scrollNode) {
+          this.scrollNode.addEventListener('scroll', this.handleScroll);
+        }
+      }
+    }
+  }
+
+>>>>>>> d1b9aa9ae8 (dependency update (#17454))
   setVisualChatLimits(
     visibleMessageLimit: number,
     combineMessageLimit: number,
