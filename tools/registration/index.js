ded38e0022319ca64077ee4a1e00f86231c2a907
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-07-21 22:17:39 +0200

    final prettier

--- tools/registration/index.js
+++ tools/registration/index.js
@@ -1,6 +1,6 @@
-const http = require('node:http');
-const { Client, GatewayIntentBits } = require('discord.js');
-const { port, token, guildId, roleId } = require('./config.json');
+const http = require("node:http");
+const { Client, GatewayIntentBits } = require("discord.js");
+const { port, token, guildId, roleId } = require("./config.json");
 
 async function main() {
   const client = new Client({ intents: [] });
@@ -8,21 +8,21 @@ async function main() {
 
   const guild = await client.guilds.fetch(guildId);
   if (!guild) {
-    console.error('ERROR in config, unable to resolve guild ' + guildId);
+    console.error("ERROR in config, unable to resolve guild " + guildId);
     return;
   }
 
   const role = await guild.roles.fetch(roleId);
   if (!role) {
-    console.error('ERROR in config, invalid role ID ', roleId);
+    console.error("ERROR in config, invalid role ID ", roleId);
     return;
   }
 
   const server = http.createServer({}, (req, res) => {
     const url = req.url;
     if (!url) {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ data: 'error parsing URL' }));
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ data: "error parsing URL" }));
       return;
     }
 
@@ -30,22 +30,22 @@ async function main() {
     try {
       urlObj = new URL(`http://localhost${url}`);
     } catch (err) {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ data: 'error parsing URL: ' + err }));
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ data: "error parsing URL: " + err }));
       return;
     }
     if (!urlObj.search) {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ data: 'error parsing URL' }));
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ data: "error parsing URL" }));
       return;
     }
 
     const params = urlObj.searchParams;
-    const member_id = params.get('member');
+    const member_id = params.get("member");
 
     if (!member_id) {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ data: 'error parsing URL' }));
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ data: "error parsing URL" }));
       return;
     }
 
@@ -53,24 +53,24 @@ async function main() {
     try {
       memberIdRaw = JSON.parse(member_id);
     } catch (err) {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ data: 'error parsing URL: ' + err }));
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ data: "error parsing URL: " + err }));
       return;
     }
 
     guild.members.addRole({
       user: memberIdRaw,
       role,
-      reason: 'SS13 Registration',
+      reason: "SS13 Registration",
     });
-    console.log('Successfully registered ', memberIdRaw);
+    console.log("Successfully registered ", memberIdRaw);
 
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ data: 'success' }));
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ data: "success" }));
   });
 
   server.listen(port);
-  console.log('Server running on port', port);
+  console.log("Server running on port", port);
 }
 
 main();
