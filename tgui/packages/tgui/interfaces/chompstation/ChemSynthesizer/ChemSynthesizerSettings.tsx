37babf47ade77da6dfef0e19671586a9a8f38f3d
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-09-02 11:01:32 +0200

    rid of useless margin (#11561)

--- tgui/packages/tgui/interfaces/chompstation/ChemSynthesizer/ChemSynthesizerSettings.tsx
+++ tgui/packages/tgui/interfaces/chompstation/ChemSynthesizer/ChemSynthesizerSettings.tsx
@@ -23,11 +23,7 @@ export const ChemSynthesizerSettings = (props) => {
       <Stack.Item height={0} grow>
         <Section height="100%" title="Settings" overflowY="auto">
           <Stack direction="column">
-            <Stack.Item
-              style={{
-                marginLeft: '0.5em', // Remove if tgui core implements gap
-              }}
-            >
+            <Stack.Item>
               <Button
                 color={production_mode ? 'green' : 'bad'}
                 icon="wrench"
